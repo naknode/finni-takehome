b775f4d6b7e072b8d6c88bca199a5d1c8bb40a55
Dan Jasnowski <d.jasnowski@gmail.com>
2024-03-11 17:58:58 -0500

    Add preline UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['node_modules/preline/dist/*.js'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('preline/plugin')]
 }
