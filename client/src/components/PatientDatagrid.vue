d227334716064bf5794a233dde1ea030e9241ad9
Dan Jasnowski <d.jasnowski@gmail.com>
2024-03-14 00:48:23 -0500

    Add first city found to column next to last name and remove searchable city at bottom

--- client/src/components/PatientDatagrid.vue
+++ client/src/components/PatientDatagrid.vue
@@ -7,28 +7,18 @@
       :pagination="true"
       :paginationPageSize="25"
       :paginationPageSizeSelector="[10, 25, 50, 100]"
-      :rowData="filteredData"
+      :rowData="props.data"
       class="ag-theme-alpine"
       style="height: 100%"
       :defaultColDef="defaultColDef"
       :animateRows="true"
     ></ag-grid-vue>
-    <div class="flex space-x-4 mt-7">
-      <div class="w-1/4">
-        <input
-          type="text"
-          placeholder="Search patients by city..."
-          v-model="query"
-          class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { format, differenceInYears } from 'date-fns'
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import { AgGridVue } from 'ag-grid-vue3'
 import 'ag-grid-community/styles/ag-grid.css'
 import type { RowClickedEvent, ValueFormatterParams, ValueGetterParams } from 'ag-grid-community'
@@ -37,8 +27,6 @@ import 'ag-grid-community/styles/ag-theme-alpine.css'
 import statusCellRenderer from './renderer/statusCellRenderer'
 import type { Patient } from 'shared'
 import router from '../router'
-
-const query = ref()
 
 const defaultColDef = ref({
   filter: 'agTextColumnFilter',
@@ -51,18 +39,6 @@ const gridOptions = {
   })
 }
 
-const filteredData = computed(() => {
-  if (!query.value) {
-    return props.data
-  }
-
-  return props.data.filter((patient) => {
-    return patient.addresses.some((address) =>
-      address.city.toLowerCase().includes(query.value.toLowerCase())
-    )
-  })
-})
-
 interface Props {
   data: Patient[]
 }
@@ -73,6 +49,19 @@ const columnDefs = [
   { headerName: 'First Name', field: 'firstName', flex: 1, filter: true },
   { headerName: 'Middle Name', field: 'middleName', filter: true },
   { headerName: 'Last Name', field: 'lastName', flex: 1, filter: true },
+  {
+    headerName: 'City',
+    field: 'addresses',
+    valueGetter: (params: ValueGetterParams) => {
+      if (params.data.addresses && params.data.addresses.length > 0) {
+        return params.data.addresses[0].city
+      } else {
+        return '--'
+      }
+    },
+    flex: 1,
+    filter: true
+  },
   {
     headerName: 'Date of Birth',
     field: 'dateOfBirth',
@@ -98,7 +87,7 @@ const onRowClicked = (event: RowClickedEvent) => {
 </script>
 <style scoped>
 .grid-container {
-  height: calc(100vh - 275px);
+  height: calc(100vh - 200px);
 }
 
 .full-height-grid {
